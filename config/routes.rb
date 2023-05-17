e0911ad9249d8aa3f5e4c2753079dfe5cc1fba45
Reilly Miller <reillyjmiller31@gmail.com>
2023-05-16 18:01:41 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       resources :vendors, only: [:show, :create, :update, :destroy]
-      resources :market_vendors, only: [:index]
+      resources :market_vendors, only: [:create, :destroy]
       resources :markets, only: [:index, :show, :create, :update, :destroy] do
         resources :vendors, only: [:index]
       end
