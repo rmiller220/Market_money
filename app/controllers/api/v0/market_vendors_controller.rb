2f244b5a66c7dafaf53992826aea946aadd1f728
Reilly Miller <reillyjmiller31@gmail.com>
2023-05-17 16:29:46 -0600

    make create and destroy actions with error message

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -1,19 +1,13 @@
 class Api::V0::MarketVendorsController < ApplicationController
-  # def index
-  #   render json: MarketVendor.all
-  # end
-
-  # def show
-  #   render json: MarketVendor.find(params[:id])
-  # end
 
   def create
     vendor = Vendor.find_by_id(params[:vendor_id])
     market = Market.find_by_id(params[:market_id])
     market_vendor = MarketVendor.new(vendor: vendor, market: market)
+
     if market_vendor.save
       render json: MarketVendorSerializer.new(market_vendor), status:201
-    else
+    elsif vendor.nil? || market.nil?
       render json:  {
         "errors": [
           {
@@ -21,22 +15,32 @@ class Api::V0::MarketVendorsController < ApplicationController
           }
         ]
       }, status: 400
+      else
+        render json:  {
+          "errors": [
+            {
+              "detail": "Validation failed: Market vendor asociation between market with market_id=#{market.id} and vendor_id=#{vendor.id} already exists"
+            }
+          ]
+        }, status: 422
     end
   end
 
   def destroy
-    market_vendor = MarketVendor.find_by_id(params[:id])
-    
+    market = Market.find_by_id(params[:market_id])
+    vendor = Vendor.find_by_id(params[:vendor_id])
+    market_vendor = MarketVendor.find_market_vendor(market.id, vendor.id)
+
     if market_vendor.nil?
       render json: {
         "errors": [
           {
-            "detail": "Couldn't find MarketVendor with 'id'=#{params[:id]}"
+            "detail": "No market vendor asociation between market with market_id=#{market.id} and vendor_id=#{vendor.id} exists"
           }
         ]
-      }
+      }, status: 404
     else
-      render json: MarketVendorSerializer.new(market_vendor), status: 200
+      render json: MarketVendorSerializer.new(MarketVendor.destroy(market_vendor.id)), status: 204
     end
   end
 end
