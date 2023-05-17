a6e2e709078863e409218f4ea45cacac0dbbc18c
Reilly Miller <reillyjmiller31@gmail.com>
2023-05-16 18:02:55 -0600

    add create action to controller, start of destroy

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -1,9 +1,42 @@
 class Api::V0::MarketVendorsController < ApplicationController
-  def index
-    render json: MarketVendor.all
+  # def index
+  #   render json: MarketVendor.all
+  # end
+
+  # def show
+  #   render json: MarketVendor.find(params[:id])
+  # end
+
+  def create
+    vendor = Vendor.find_by_id(params[:vendor_id])
+    market = Market.find_by_id(params[:market_id])
+    market_vendor = MarketVendor.new(vendor: vendor, market: market)
+    if market_vendor.save
+      render json: MarketVendorSerializer.new(market_vendor), status:201
+    else
+      render json:  {
+        "errors": [
+          {
+            "detail": "#{market_vendor.errors.full_messages.to_sentence}"
+          }
+        ]
+      }, status: 400
+    end
   end
 
-  def show
-    render json: MarketVendor.find(params[:id])
+  def destroy
+    market_vendor = MarketVendor.find_by_id(params[:id])
+    
+    if market_vendor.nil?
+      render json: {
+        "errors": [
+          {
+            "detail": "Couldn't find MarketVendor with 'id'=#{params[:id]}"
+          }
+        ]
+      }
+    else
+      render json: MarketVendorSerializer.new(market_vendor), status: 200
+    end
   end
 end
