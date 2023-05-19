60cf384d25f5e18357f4b5c9c642eebc8667e4c1
Reilly Miller <reillyjmiller31@gmail.com>
2023-05-18 18:41:37 -0600

    edit poros initialize

--- app/poros/atm.rb
+++ app/poros/atm.rb
@@ -1,12 +1,13 @@
 class Atm 
-  attr_reader :lat, :lon, :dist, :name, :address
+  attr_reader :id, :lat, :lon, :distance, :name, :address
 
   def initialize(data)
-    # require 'pry'; binding.pry
-    @lat = data[:results][0][:position][:lat]
-    @lon = data[:results][0][:position][:lon]
-    @distance = data[:results][0][:dist]
-    @name = data[:results][0][:poi][:name]
-    @address = data[:results][0][:address][:freeformAddress]
+
+      @lat = data[:position][:lat]
+      @lon = data[:position][:lon]
+      @distance = data[:dist]
+      @name = data[:poi][:name]
+      @address = data[:address][:freeformAddress]
+      @id = nil
   end
 end
